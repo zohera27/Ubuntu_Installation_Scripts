8604be79f597e5c62e55cd55f696b37efb3bc2a1
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-04-17 18:33:47 +0530

    Docker installation

--- docker.sh
+++ docker.sh
@@ -3,7 +3,11 @@ sudo apt update -y
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.gpg > /dev/null
+
+(or) Alertnate Method - curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o docker.gpg
+                        sudo mv docker.gpg /etc/apt/trusted.gpg.d/
+
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
