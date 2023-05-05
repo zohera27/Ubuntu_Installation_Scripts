fcfd23091995d319b6411b6b2ce5b252fff0b6a1
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-05-05 18:38:43 +0530

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -171,7 +171,12 @@ Get the Alertmanager URL by running these commands in the same shell:
 Get the PushGateway URL by running these commands in the same shell:
   export POD_NAME=$(kubectl get pods --namespace default -l "app=prometheus-pushgateway,component=pushgateway" -o jsonpath="{.items[0].metadata.name}")
   kubectl --namespace default port-forward $POD_NAME 9091
-
+  
+  
+  Get the Grafana URL to visit by running these commands in the same shell:
+     export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=grafana" -o jsonpath="{.items[0].metadata.name}")
+     kubectl --namespace default port-forward $POD_NAME 3000
+     
 
 To uninstall 
 
