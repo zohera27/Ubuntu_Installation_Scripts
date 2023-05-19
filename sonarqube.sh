3c65d574fa17d31f23e1a7d983185ee323c2ad62
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-05-20 00:53:42 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,3 +1,7 @@
 # INSTALL SONARQUBE
   
     docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+   
+ #Install SonarQube on Ubuntu 22.04
+ 
+ https://www.fosstechnix.com/how-to-install-sonarqube-on-ubuntu-22-04-lts/
